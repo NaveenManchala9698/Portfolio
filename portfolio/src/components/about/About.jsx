316b6b616cd38b7dead47ff4b9409301e74f017c
Naveen Kumar Manchala <100999011+NaveenManchala9698@users.noreply.github.com>
2022-12-05 00:42:21 +0100

    changes

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./About.css";
 import Image from "../../assets/avatar-2.svg";
 import Image2 from "../../assets/Naveen 3.png";
-import NaveenCV from "../../assets/NaveenCV.pdf";
+import NaveenCV from "../../assets/Naveen CV2.pdf";
 import AboutBox from "./AboutBox";
 
 const About = () => {
